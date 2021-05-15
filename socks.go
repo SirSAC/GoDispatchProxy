f18e0d44df1573e5f92cf7500d8184b2593784d2
Sergiu Alexandru Capatina <sergiu.alexandru.capatina@hotmail.com>
2021-05-15 16:06:52 +0300

    Update socks.go
    Prototype 1.

--- socks.go
+++ socks.go
@@ -7,7 +7,6 @@ import (
 	. "fmt"
 	"log"
 	. "net"
-	"time"
 )
 //
 func server_choice(local_connection Conn) (error) {
@@ -19,14 +18,15 @@ func server_choice(local_connection Conn) (error) {
 //
 func client_greeting(local_connection Conn) (byte, []byte, error) {
 	buf := make([]byte, 2)
+	network_error := New("[!] Client greeting failed")
 	if nRead, err := local_connection.Read(buf); err != nil || nRead != len(buf) {
-		return 0, nil, New("[!] Client greeting failed")
+		return 0, nil, network_error
 	}
 	ver := buf[0]
 	nauth := buf[1]
 	auth := make([]byte, nauth)
 	if nRead, err := local_connection.Read(auth); err != nil || nRead != int(nauth) {
-		return 0, nil, New("[!] Client greeting failed")
+		return 0, nil, network_error
 	}
 	return ver, auth, nil
 }
@@ -34,22 +34,24 @@ func client_greeting(local_connection Conn) (byte, []byte, error) {
 func handle_target(source_address string, source_port []byte, secure_connection bool) (uint16) {
 	destination_port := BigEndian.Uint16(source_port)
 	if secure_connection == true {
-		timeout_verify, _ := time.ParseDuration("125ms")
 		if destination_port == 80 {
-			_, err := DialTimeout("tcp", "source_address:443", timeout_verify)
+			target_connection, err := DialTimeout("tcp", Sprintf("%s:%d", source_address, 443), timeout_verify)
 			if err == nil {
+				defer target_connection.Close()
 				return 443
 			}
 		}
 		if destination_port == 21 {
-			_, err := DialTimeout("tcp", "source_address:990", timeout_verify)
+			target_connection, err := DialTimeout("tcp", Sprintf("%s:%d", source_address, 990), timeout_verify)
 			if err == nil {
+				defer target_connection.Close()
 				return 990
 			}
 		}
 		if destination_port == 20 {
-			_, err := DialTimeout("tcp", "source_address:989", timeout_verify)
+			target_connection, err := DialTimeout("tcp", Sprintf("%s:%d", source_address, 989), timeout_verify)
 			if err == nil {
+				defer target_connection.Close()
 				return 989
 			}
 		}
@@ -59,10 +61,11 @@ func handle_target(source_address string, source_port []byte, secure_connection 
 //
 func client_request(local_connection Conn, secure_connection bool) (string, error) {
 	header := make([]byte, 4)
+	network_error := New("[!] Client connection request failed")
 	if nRead, err := local_connection.Read(header); err != nil || nRead != len(header) {
 		local_connection.Write([]byte {5, GENERAL_FAILURE, 0, 1, 0, 0, 0, 0, 0, 0})
 		local_connection.Close()
-		return "", New("[!] Client connection request failed")
+		return "", network_error
 	}
 	ver := header[0]
 	if ver != SOCKS_VERSION {
@@ -89,18 +92,18 @@ func client_request(local_connection Conn, secure_connection bool) (string, erro
 			if nRead, err := local_connection.Read(domain_name_length); err != nil || nRead != len(domain_name_length) {
 				local_connection.Write([]byte {5, CONNECTION_REFUSED, 0, 1, 0, 0, 0, 0, 0, 0})
 				local_connection.Close()
-				return "", New("[!] Client connection request failed")
+				return "", network_error
 			}
 			domain_name := make([]byte, domain_name_length[0])
 			if nRead, err := local_connection.Read(domain_name); err != nil || nRead != len(domain_name) {
 				local_connection.Write([]byte {5, GENERAL_FAILURE, 0, 1, 0, 0, 0, 0, 0, 0})
 				local_connection.Close()
-				return "", New("[!] Client connection request failed")
+				return "", network_error
 			}
 			if nRead, err := local_connection.Read(dstport); err != nil || nRead != len(dstport) {
 				local_connection.Write([]byte {5, GENERAL_FAILURE, 0, 1, 0, 0, 0, 0, 0, 0})
 				local_connection.Close()
-				return "", New("[!] Client connection request failed")
+				return "", network_error
 			}
 			destination_address := Sprintf("%s", string(domain_name))
 			destination_port := handle_target(destination_address, dstport, secure_connection)
@@ -110,12 +113,12 @@ func client_request(local_connection Conn, secure_connection bool) (string, erro
 			if nRead, err := local_connection.Read(ipv6_address); err != nil || nRead != len(ipv6_address) {
 				local_connection.Write([]byte {5, CONNECTION_REFUSED, 0, 1, 0, 0, 0, 0, 0, 0})
 				local_connection.Close()
-				return "", New("[!] Client connection request failed")
+				return "", network_error
 			}
 			if nRead, err := local_connection.Read(dstport); err != nil || nRead != len(dstport) {
 				local_connection.Write([]byte {5, GENERAL_FAILURE, 0, 1, 0, 0, 0, 0, 0, 0})
 				local_connection.Close()
-				return "", New("[!] Client connection request failed")
+				return "", network_error
 			}
 			destination_address := Sprintf("[%d:%d:%d:%d:%d:%d:%d:%d]", ipv6_address[0], ipv6_address[1], ipv6_address[2], ipv6_address[3], ipv6_address[4], ipv6_address[5], ipv6_address[6], ipv6_address[7])
 			destination_port := handle_target(destination_address, dstport, secure_connection)
@@ -125,12 +128,12 @@ func client_request(local_connection Conn, secure_connection bool) (string, erro
 			if nRead, err := local_connection.Read(ipv4_address); err != nil || nRead != len(ipv4_address) {
 				local_connection.Write([]byte {5, CONNECTION_REFUSED, 0, 1, 0, 0, 0, 0, 0, 0})
 				local_connection.Close()
-				return "", New("[!] Client connection request failed")
+				return "", network_error
 			}
 			if nRead, err := local_connection.Read(dstport); err != nil || nRead != len(dstport) {
 				local_connection.Write([]byte {5, GENERAL_FAILURE, 0, 1, 0, 0, 0, 0, 0, 0})
 				local_connection.Close()
-				return "", New("[!] Client connection request failed")
+				return "", network_error
 			}
 			destination_address := Sprintf("%d.%d.%d.%d", ipv4_address[0], ipv4_address[1], ipv4_address[2], ipv4_address[3])
 			destination_port := handle_target(destination_address, dstport, secure_connection)
